c81b258d3eb6b286c588f4b7abf81ecb449ff8e4
Marcus Baptiste <marbap45@gmail.com>
2018-05-13 19:58:23 -0400

    Add server functionality

--- server.js
+++ server.js
@@ -6,10 +6,16 @@ var server = http.createServer(function(req, res) {
   var userInfo = {};  
 
   //get user information
-  userInfo.ipAddress = 0;
-  userInfo.lang = '';
-  userInfo.os = '';  
+  userInfo.ipaddress = req.connection.remoteAddress;
 
+  //get first entry before comman in accept-language header
+  //this is supposed to be the language setting
+  userInfo.language  = req.headers['accept-language'].split(',')[0];
+
+  //find first entry in parameters in user-agent string and strip parentheses
+  //this is supposed to be the operating system
+  userInfo.software  = req.headers['user-agent'].match(/\([^\(\)]*\)/g)[0].replace(/[\(\)]/g, '');
+  
   //send json response
   res.writeHead(200, { 'Content-Type' : 'text/plain' });
   res.end(JSON.stringify(userInfo));
